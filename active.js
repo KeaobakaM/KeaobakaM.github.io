b05ab6c2c6512461d20df80c3b64b07d3959fe0c
KeaobakaM <keaobaka.km@gmail.com>
2023-05-04 21:03:52 +0200

    Close menu on click

--- active.js
+++ active.js
@@ -1,8 +1,33 @@
 
 const navLinks = document.querySelectorAll('.nav-link');
+const menu = document.querySelector('.checkbtn');
+const navbar = document.querySelector('.navbar');
+const screenSize = window.matchMedia("screen and (max-device-width: 750px)");
+
+screenSize.addEventListener("change", e => {
+   if(e.matches){
+menu.addEventListener('click', () => {
+   navbar.style.display = 'initial';
+})
+
 navLinks.forEach(link => {
    link.addEventListener('click', () => {
+    navbar.style.display = 'initial';
     document.querySelector('.active')?.classList.remove('active');
     link.classList.add('active');
+    link.classList.remove('active');
+    if(navbar.style.display = 'initial'){
+      navbar.style.display = 'none';
+    }
+    
    }) 
+})
+} else {
+     navbar.style.display = 'initial';
+     navLinks.forEach(link => {
+      link.addEventListener('click', () => {
+       navbar.style.display = 'initial';
+      })
+   })
+ }
 })
