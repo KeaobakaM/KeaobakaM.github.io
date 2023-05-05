b2d47edd5091bb809944e4a8662badb76801762f
KeaobakaM <keaobaka.km@gmail.com>
2023-05-05 17:05:18 +0200

    Changing to adapt to screen sizes

--- active.js
+++ active.js
@@ -1,33 +1,72 @@
-
-const navLinks = document.querySelectorAll('.nav-link');
-const menu = document.querySelector('.checkbtn');
-const navbar = document.querySelector('.navbar');
+const navLinks = document.querySelectorAll(".nav-link");
+const menu = document.querySelector(".checkbtn");
+const navbar = document.querySelector(".navbar");
 const screenSize = window.matchMedia("screen and (max-device-width: 750px)");
+const bigScreen = window.matchMedia("screen and (min-width: 750px)");
 
-screenSize.addEventListener("change", e => {
-   if(e.matches){
-      navbar.style.display = 'none';
-menu.addEventListener('click', () => {
-   navbar.style.display = 'initial';
-})
-
-navLinks.forEach(link => {
-   link.addEventListener('click', () => {
-    navbar.style.display = 'initial';
-    document.querySelector('.active')?.classList.remove('active');
-    link.classList.add('active');
-    if(navbar.style.display = 'initial'){
-      navbar.style.display = 'none';
-    }
-    
-   }) 
-})
-} else {
-     navbar.style.display = 'initial';
-     navLinks.forEach(link => {
-      link.addEventListener('click', () => {
-       navbar.style.display = 'initial';
-      })
+navbar.style.display = "none";
+if(bigScreen.matches === true){
+   navbar.style.display = "initial";
+   navLinks.forEach((link) => {
+   link.addEventListener("click", () => {
+      document.querySelector('.active')?.classList.remove('active');
+      link.classList.add("active");
+      navbar.style.display = "initial";
    })
- }
-})
+    });
+    
+}
+
+    menu.addEventListener("click", () => {
+      navbar.style.display = "initial";
+    });
+
+    navLinks.forEach((link) => {
+      link.addEventListener("click", () => {
+        navbar.style.display = "initial";
+        document.querySelector('.active')?.classList.remove('active');
+        link.classList.add("active");
+        if ((navbar.style.display = "initial") && (screenSize.matches)) {
+          navbar.style.display = "none";
+        }
+      });
+    });
+
+screenSize.addEventListener("change", (e) => {
+  if (e.matches) {
+    navbar.style.display = "none";
+    menu.addEventListener("click", () => {
+      navbar.style.display = "initial";
+    });
+
+    navLinks.forEach((link) => {
+      link.addEventListener("click", () => {
+        navbar.style.display = "initial";
+        document.querySelector('.active')?.classList.remove('active');
+        link.classList.add("active");
+        if ((navbar.style.display = "initial")) {
+          navbar.style.display = "none";
+        }
+      });
+    });
+  } else {
+    navbar.style.display = "initial";
+    navLinks.forEach((link) => {
+      link.addEventListener("click", () => {
+        link.classList.add("active");
+        navbar.style.display = "initial";
+      });
+    });
+  }
+});
+
+bigScreen.addEventListener("change", (e) => {
+  if (e.matches) {
+    navbar.style.display = "initial";
+    navLinks.forEach((link) => {
+      link.addEventListener("click", () => {
+        link.classList.add("active");
+      });
+    });
+  }
+});
